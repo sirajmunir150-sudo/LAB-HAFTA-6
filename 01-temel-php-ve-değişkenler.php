36216c92110949e342226e6dc975a0be77141a74
sirajmunir150-sudo <sirajmunir150@gmail.com>
2025-10-23 10:40:07 +0300

    Add basic PHP script with variables and output

---
+++ 01-temel-php-ve-değişkenler.php
@@ -0,0 +1,14 @@
+<?php
+// Ekrana Merhaba Dünya yazdırma
+echo "Merhaba Dünya!";
+
+// Değişken tanımlama ve kullanma
+$mesaj = "PHP öğreniyorum.";
+$yil = 2025;
+$pi_sayisi = 3.14;
+$ogrenci_mi = true;
+
+echo "<br>$mesaj<br>";
+echo "Bu yıl " . $yil . " yılıdır.<br>";
+echo "Pi sayısı yaklaşık olarak $pi_sayisi değerindedir.";
+?>
