bb1e6f6906d32ceb1712e135c55280db7550bd84
sirajmunir150-sudo <sirajmunir150@gmail.com>
2025-10-23 10:51:06 +0300

    Add switch-case structure for day names

---
+++ Switch-case.php
@@ -0,0 +1,16 @@
+<?php
+$gun = "Car";
+switch ($gun) {
+    case "Pzt":
+        echo "Pazartesi";
+        break;
+    case "Sal":
+        echo "Salı";
+        break;
+    case "Car":
+        echo "Çarşamba";
+        break;
+    default:
+        echo "Bilinmeyen gün";
+}
+?>
